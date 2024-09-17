65a66ab61f3d747764d6fa0d8363f05f81ab9b3f
Nicolayys <2747288527@qq.com>
2024-09-17 22:17:22 +0800

    1

--- QTableDemo/PageWidget.h
+++ QTableDemo/PageWidget.h
@@ -2,7 +2,23 @@
 #define PAGEWIDGET_H
 
 #include <QWidget>
-
+#include <QTableView>
+#include <QToolButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QHeaderView>
+#include <QStandardItemModel>
+#include <QtConcurrent/QtConcurrent>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QIntValidator>
+#include <QListWidget>
+#include <QPushButton>
+#include <QMouseEvent>
+#include <QPainter>
 namespace Ui {
 class PageWidget;
 }
